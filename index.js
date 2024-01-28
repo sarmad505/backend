75644194d711995bb4b7bd223b35c0d9cc0874df
sarmad505 <sarmadbaig505@gmail.com>
2024-01-28 16:02:58 +0500

    backend chat app

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+try{
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username: username, secret: username, first_name: username},
+        {headers:{"private-key":"5a620d6a-3659-4946-98c8-377e8189d53e"}}
+    )
+    return res.status(r.status).json(r.data)
+}catch(e){
+    return res.status(e.response.status).json(e.response.data)
+}
+
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001);
